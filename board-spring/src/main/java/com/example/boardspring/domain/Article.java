e70e59c9d0f757c9b12322782a506ec716a46eee
dev_kei <kei3824@naver.com>
2022-09-07 23:49:00 +0900

    #8 - ADD domain
    add domain

--- board-spring/src/main/java/com/example/boardspring/domain/Article.java
+++ board-spring/src/main/java/com/example/boardspring/domain/Article.java
@@ -1,14 +1,62 @@
 package com.example.boardspring.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String hashtag;
+    @Setter @Column(nullable = false) private String title;
+    @Setter private String hashtag;
+    @Setter @Column(nullable = false, length = 10000) private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    private Article(String title, String hashtag, String content) {
+        this.title = title;
+        this.hashtag = hashtag;
+        this.content = content;
+    }
+
+    private Article of(String title, String hashtag, String content) {
+        return new Article(title, content, hashtag);
+    }
+
+    protected Article() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- board-spring/src/main/java/com/example/boardspring/domain/ArticleComment.java
+++ board-spring/src/main/java/com/example/boardspring/domain/ArticleComment.java
@@ -1,14 +1,67 @@
 package com.example.boardspring.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+
+@Entity
 public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Article article;
-    private String title;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @ManyToOne(optional = false) private Article article;
+    @Setter @Column(nullable = false, length = 255) private String content;
+
+    @CreatedDate
+    @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100) private String modifiedBy;
+
+    protected ArticleComment() {}
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+
 }
